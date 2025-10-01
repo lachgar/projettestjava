19fc7176cb54b324fe8c5a61f62798f11827b6a7
unknown <lachgar.m@gmail.com>
2025-10-01 13:36:10 +0200

    1er commit

---
+++ src/ma/projet/test/Test.java
@@ -0,0 +1,15 @@
+package ma.projet.test;
+
+import java.util.Scanner;
+
+public class Test {
+	
+	public static void main(String[] args) {
+		System.out.println("Donner un entier :");
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		
+		System.out.println(n);
+	}
+
+}
