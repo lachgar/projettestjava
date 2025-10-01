5d4e705153f39cc7953925835feb5bb39362a5a8
unknown <lachgar.m@gmail.com>
2025-10-01 13:47:23 +0200

    1er commit

---
+++ src/ma/projet/test/TestTableau.java
@@ -0,0 +1,17 @@
+package ma.projet.test;
+
+public class TestTableau {
+
+	public static void main(String[] args) {
+
+		int t[] = new int[5];
+
+		for (int i : t)
+			System.out.println(i);
+
+		for (int i = 0; i < t.length; i++)
+			System.out.println(t[i]);
+
+	}
+
+}
